5c123ff90fa7b13819d8bc1f9cfaa3d487b61ca4
Саня <@DESKTOP-M02BDTH>
2022-01-05 19:16:03 +0700

    Добавьте файлы проекта.

---
+++ WpfApp1/CreateWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateWindow.xaml
+    /// </summary>
+    public partial class CreateWindow : Window
+    {
+        public CreateWindow()
+        {
+            InitializeComponent();
+            Return.Click += GoReturn;
+            NewFamilyButton.Click += GoToFamily;
+        }
+        private void GoReturn(Object sender, EventArgs e)
+        {
+            
+            CreateW.Close();
+            CreateW.Owner.Visibility = Visibility.Visible;
+        }
+
+        private void GoToFamily(Object sender, EventArgs e)
+        {
+
+            CreateW.Close();
+            Family window = new Family();
+            window.Owner = CreateW.Owner;
+            window.ShowDialog();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateGhostButton.Click += GoCreate;
+            GoToFamilyButton.Click += GoFindFamily;
+            Exit.Click += GoExit;
+        }
+
+        private void GoCreate(Object sender, EventArgs e)
+        {
+            MainW.Visibility = Visibility.Hidden;
+            CreateWindow window = new CreateWindow();
+            window.Owner = this;
+            window.ShowDialog();
+        }
+
+        private void GoFindFamily(Object sender, EventArgs e)
+        {
+            FindFamilyWindow window = new FindFamilyWindow();
+            window.Owner = this;
+            window.ShowDialog();
+        }
+
+        private void GoExit(Object sender, EventArgs e)
+        {
+            MainW.Close();
+        }
+    }
+}
